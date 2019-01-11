02f77283537ca15c9d82bad70a7cb6eefd86f8d1
kritsanrongkaew <dumtk1@gmail.com>
2019-01-11 10:40:37 +0700

    Develop next

--- app/src/main/java/com/computersoftware/week12/fragmentweek12/Fragment/FragmentSecond.java
+++ app/src/main/java/com/computersoftware/week12/fragmentweek12/Fragment/FragmentSecond.java
@@ -4,19 +4,38 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.computersoftware.week12.fragmentweek12.R;
 
 public class FragmentSecond extends Fragment {
+
+    Button btn_back;
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.second_layout,container,false);
+
+            btn_back = view.findViewById(R.id.btn_back);
+            btn_back.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    FragmentManager manager = getActivity().getSupportFragmentManager();
+//                    manager.popBackStack();
+                    FragmentTransaction transaction = manager.beginTransaction();
+                    transaction.replace(R.id.contentFrg,new FragmentFrist())
+                            .addToBackStack(null).commit();
+                }
+            });
 
         return view;
     }
--- app/src/main/java/com/computersoftware/week12/fragmentweek12/MainActivity.java
+++ app/src/main/java/com/computersoftware/week12/fragmentweek12/MainActivity.java
@@ -1,9 +1,12 @@
 package com.computersoftware.week12.fragmentweek12;
 
+import android.content.DialogInterface;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.computersoftware.week12.fragmentweek12.Fragment.FragmentFrist;
 import com.computersoftware.week12.fragmentweek12.Fragment.FragmentSecond;
@@ -21,6 +24,36 @@ public class MainActivity extends AppCompatActivity {
         manager = getSupportFragmentManager();
         transaction = manager.beginTransaction();
         transaction.replace(R.id.contentFrg,new FragmentFrist()).addToBackStack(null).commit();
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        int frgNumber = manager.getBackStackEntryCount();
+
+        if(frgNumber > 1){
+            manager.popBackStack();
+        }else {
+            AlertDialog.Builder builder
+                    = new AlertDialog.Builder(MainActivity.this);
+            builder.setMessage("คุณต้องการออกจากแอพหรือไม่");
+            builder.setPositiveButton("ตกลง", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int i) {
+                    finish();
+                }
+            });
+
+            builder.setNegativeButton("ยกเลิก", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int i) {
+                    dialog.dismiss();
+                }
+            });
+            builder.show();
+        }
+
+        Toast.makeText(this, ""+frgNumber, Toast.LENGTH_SHORT).show();
 
     }
 }
